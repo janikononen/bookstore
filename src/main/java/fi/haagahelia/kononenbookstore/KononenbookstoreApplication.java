ad18ad346569687cd7628155f25808a1016cfd14
janikononen <bhm556@myy.haaga-helia.fi>
2025-03-31 21:50:11 +0300

    lisätään käyttäjät tietokantaan

--- src/main/java/fi/haagahelia/kononenbookstore/KononenbookstoreApplication.java
+++ src/main/java/fi/haagahelia/kononenbookstore/KononenbookstoreApplication.java
@@ -25,23 +25,23 @@ public class KononenbookstoreApplication {
 			UserRepository uRepository, PasswordEncoder encoder) {
 		return (args) -> {
 
-			// Category it = new Category("IT");
-			// Category kauhu = new Category("Kauhu");
-			// Category talous = new Category("Talous");
+			Category it = new Category("IT");
+			Category kauhu = new Category("Kauhu");
+			Category talous = new Category("Talous");
 
-			// cRepository.save(it);
-			// cRepository.save(kauhu);
-			// cRepository.save(talous);
+			cRepository.save(it);
+			cRepository.save(kauhu);
+			cRepository.save(talous);
 
-			// bRepository.save(new Book("test", "test", "test", 2012, 20.5, it));
-			// bRepository.save(new Book("test2", "test2", "test2", 2013, 25.5, kauhu));
-			// bRepository.save(new Book("test3", "test3", "test3", 2014, 30.5, talous));
+			bRepository.save(new Book("test", "test", "test", 2012, 20.5, it));
+			bRepository.save(new Book("test2", "test2", "test2", 2013, 25.5, kauhu));
+			bRepository.save(new Book("test3", "test3", "test3", 2014, 30.5, talous));
 
-			// User user1 = new User("user", encoder.encode("u@123"), "USER");
-			// User user2 = new User("admin", encoder.encode("a@123"), "ADMIN");
+			User user1 = new User("user", encoder.encode("u@123"), "USER");
+			User user2 = new User("admin", encoder.encode("a@123"), "ADMIN");
 
-			// uRepository.save(user1);
-			// uRepository.save(user2);
+			uRepository.save(user1);
+			uRepository.save(user2);
 		};
 	};
 }
